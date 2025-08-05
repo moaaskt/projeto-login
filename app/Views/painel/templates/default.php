8c37d1dc8c403a39587e55c419407ac43882cabd
moaaskt <moacirmayer@hotmail.com>
2025-08-05 17:44:01 -0300

    edit final da view template

--- app/Views/painel/templates/default.php
+++ app/Views/painel/templates/default.php
@@ -109,7 +109,7 @@
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
     <script src="<?= base_url('js/dashboard.js') ?>"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <?= $this->renderSection('scripts') ?>
     <?= $this->section('pageScripts') ?>
 <?= $this->endSection() ?>
